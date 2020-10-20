92dec42b83d7550168888bb57e1ebde7cf3c8499
CristianoNunes <cristianonunes99@gmail.com>
2020-10-20 17:45:54 -0300

    Requisito 3 - FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+  const { history, getByText } = renderWithRouter(<App />);
+  history.push('/favorites');
+  expect(history.location.pathname).toBe('/favorites');
+  expect(getByText(/No favorite/)).toBeInTheDocument();
+  expect(getByText(/pokemon found/)).toBeInTheDocument();
+});
