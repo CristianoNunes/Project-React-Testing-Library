509b4df1a0e41b0b7feadd14bfcba57524707511
CristianoNunes <cristianonunes99@gmail.com>
2020-10-20 16:59:40 -0300

    Requisito 2 - About

--- src/renderWithRouter.js
+++ src/renderWithRouter.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { render } from '@testing-library/react';
 
 const renderWithRouter = (component) => {
   const history = createMemoryHistory();
   return ({
-    ...render(<MemoryRouter history={ history }>{component}</MemoryRouter>), history,
+    ...render(<Router history={ history }>{component}</Router>), history,
   });
 };
 
--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,31 @@
-test('', () => {});
+import React from 'react';
+import { getByAltText, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testes do About:', () => {
+  it('Teste se a página contém as informações sobre a Pokédex.', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    expect(history.location.pathname).toBe('/about');
+    const information = screen.getByText(/This application simulates a Pokédex/)
+    expect(information).toBeInTheDocument();
+  });
+
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/about');
+    expect(history.location.pathname).toBe('/about');
+    const h2 = screen.getAllByRole('heading', { level: 2 });
+    expect(h2[1]).toHaveTextContent('About Pokédex')
+  });
+
+  it('Teste se a página contém a seguinte imagem de uma Pokédex', async () => {
+    const { getByRole, history } = renderWithRouter(<App />);
+    history.push('/about');
+    const img = getByRole('img');
+    const srcImg = 'https://cdn.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    expect(img.src).toBe(srcImg);
+  });
+});
+
