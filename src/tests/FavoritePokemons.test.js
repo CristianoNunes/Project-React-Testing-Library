c1ee34ce43180f6d346bc040a803b5d2ee1c36b9
CristianoNunes <cristianonunes99@gmail.com>
2020-10-21 19:32:55 -0300

    Corrigindo o FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
 
-it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
-  const { history, getByText } = renderWithRouter(<App />);
-  history.push('/favorites');
-  expect(history.location.pathname).toBe('/favorites');
-  expect(getByText(/No favorite/)).toBeInTheDocument();
-  expect(getByText(/pokemon found/)).toBeInTheDocument();
+describe('Testes do About:', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const { getByText } = renderWithRouter(<FavoritePokemons />);
+    expect(getByText(/No favorite/)).toBeInTheDocument();
+    expect(getByText(/pokemon found/)).toBeInTheDocument();
+  });
+
+  it('Teste se é exibido todos os cards de pokémons favoritados', () => {
+    const { getByText, queryByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ [pokemons[2]] } />
+    );
+    const poke3 = getByText('Caterpie');
+    const poke4 = queryByText('Ekans');
+    expect(poke3).toBeInTheDocument();
+    expect(poke4).not.toBeInTheDocument();
+  });
+
+  it('Teste se Não é exibido nenhum card de pokémon não favoritado', () => {
+    const { queryByText } = renderWithRouter(
+      <FavoritePokemons pokemons={ [] } />
+    );
+    const poke4 = queryByText('Ekans');
+    expect(poke4).not.toBeInTheDocument();
+  });
+
 });
