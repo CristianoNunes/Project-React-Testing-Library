196b69abbb609567b68917d5c8e16abb11f577ca
CristianoNunes <cristianonunes99@gmail.com>
2020-10-26 16:40:32 -0300

    Requisito 6 - Pokemon

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,37 +1,73 @@
 import React from 'react';
-import renderWithRouter from '../renderWithRouter';
-import App from '../App';
+import { fireEvent } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import Pokemon from '../components/Pokemon';
+import pokemons from '../data';
 
-describe('Testes de Pokemon:', () => {
-  it('O nome correto do Pokémon deve ser mostrado na tela', () => {
-    const { getByTestId } = renderWithRouter(<App />);
-    const pName = getByTestId('pokemon-name');
-    expect(pName).toHaveTextContent('Pikachu');
+describe('Testando o arquivo Pokemon.js', () => {
+  it('1- É renderizado um card com as informações de determinado pokémon', () => {
+    const { getByTestId, getAllByRole } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemons[0] }
+        isFavorite={ { 25: false } }
+      />,
+    );
+    const namePkm = getByTestId('pokemon-name');
+    const typePkm = getByTestId('pokemonType');
+    const weight = getByTestId('pokemon-weight');
+    const image = getAllByRole('img');
+    const srcImage = 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
+    expect(namePkm).toHaveTextContent('Pikachu');
+    expect(typePkm).toHaveTextContent('Electric');
+    expect(weight).toHaveTextContent('Average weight:6.0kg');
+    expect(image[0].src).toBe(srcImage);
+    expect(image[0].alt).toBe('Pikachu sprite');
   });
 
-  it('O Tipo correto do Pokémon deve ser mostrado na tela', () => {
-    const { getByTestId } = renderWithRouter(<App />);
-    const pType = getByTestId('pokemonType');
-    expect(pType).toHaveTextContent('Electric');
-  });
-
-  it('O Peso correto do Pokémon deve ser mostrado na tela', () => {
-    const { getByTestId } = renderWithRouter(<App />);
-    const pWeight = getByTestId('pokemon-weight');
-    expect(pWeight).toHaveTextContent('Average weight:6.0kg');
-  });
-
-  it('O Link correto do Pokémon de acordo com id', () => {
-    const { getByRole } = renderWithRouter(<App />);
+  it('2-O Pokémon da Pokédex contém um link de navegação para exibir detalhes', () => {
+    const { getByRole } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemons[0] }
+        isFavorite={ { 25: false } }
+      />,
+    );
     const link = getByRole('link', { name: 'More details' });
+    expect(link).toBeInTheDocument();
     expect(link.href.split('http://localhost')[1]).toBe('/pokemons/25');
   });
 
-  it('Teste se página mostra a imagem', () => {
-    const { getAllByRole } = renderWithRouter(<App />);
-    const img = getAllByRole('img');
-    const srcImg = 'https://cdn.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png';
-    expect(img[0].src).toBe(srcImg);
-    expect(img[0].alt).toBe('Pikachu sprite');
+  it('3-clicando no link de navegação é redirecionado para os detalhes', () => {
+    const { getByRole } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemons[0] }
+        isFavorite={ { 25: false } }
+      />,
+    );
+    const link = getByRole('link', { href: '/pokemons/25' });
+    expect(link).toBeInTheDocument();
+  });
+
+  it('4-A URL exibida no navegador muda para /pokemon/<id>', () => {
+    const { getByRole, history } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemons[0] }
+        isFavorite={ { 25: false } }
+      />,
+    );
+    const link = getByRole('link', { href: '/pokemons/25' });
+    fireEvent.click(link);
+    expect(history.location.pathname).toBe('/pokemons/25');
+  });
+
+  it('5-Existe um ícone de estrela nos Pokémons favoritados', () => {
+    const { getAllByRole } = renderWithRouter(
+      <Pokemon
+        pokemon={ pokemons[0] }
+        isFavorite={ { 25: true } }
+      />,
+    );
+    const images = getAllByRole('img');
+    expect(images[1].src.split('http://localhost')[1]).toBe('/star-icon.svg');
+    expect(images[1].alt).toBe('Pikachu is marked as favorite');
   });
 });
